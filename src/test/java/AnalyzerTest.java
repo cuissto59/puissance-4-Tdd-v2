8f4695f7935e48b331ad65d1f705ee968e737e56
Lahrizi Taha <taha.lahrizi@uit.ac.ma>
2022-03-16 10:48:31 +0100

    new commit

---
+++ src/test/java/AnalyzerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AnalyzerTest {
+
+
+
+}
